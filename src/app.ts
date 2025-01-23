fdaec48cdda421bba280b6fc8eb5a4fd1277f69d
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-01-23 14:45:19 -0300

    feat: create function to database connection

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,14 @@
 import express from 'express'
+import * as mysql from 'mysql2/promise'
+
+function createConnection() {
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'tickets'
+    })
+}
 
 const app = express()
 
